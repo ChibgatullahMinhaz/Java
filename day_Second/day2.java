05de7bacff5510b8f457106bc419aecc313e880a
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-03 13:20:13 +0600

    how to get input from users

--- day_Second/day2.java
+++ day_Second/day2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 // output in 3ways
 class secondDay {
     public static void main(String[] args){
@@ -19,4 +20,17 @@ class secClass {
 
     }
 }
+// quize using variables
+// how to get input in users
+class ThirdClass {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+     int sum = a + b;
+     System.out.println(sum);
+     sc.close();
+    }
+}
+
 
