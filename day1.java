7f32f59ca03ee26ede2fe90e1cf4b5f3e6ccd193
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-03 12:23:37 +0600

    pettern output using print()

--- day1.java
+++ day1.java
@@ -1,6 +1,21 @@
 
  class FirstClass {
     public static void main(String[] args){
-        System.err.println("Hello World");
+        System.out.println("Hello World");
+        System.err.println(3 + 3);
+        System.err.println(100 * 2);
+
+    }
+}
+// output in 3ways
+class secondDay {
+    public static void fistWay(String[] args){
+        System.out.print("hello World");
+    }
+    public static void secWay(String[] args){
+        System.out.print("hello World");
+    }
+    public static void thirdWay(String[] args){
+        System.out.print("hello World/");
     }
 }
---
+++ day_Second/day2.java
@@ -0,0 +1,22 @@
+// output in 3ways
+class secondDay {
+    public static void main(String[] args){
+        System.out.print("hello World"); //1no way
+        System.out.print("hello World\n"); // 2no way
+        System.out.println("hello World"); //3no way
+        System.out.print("hello World");
+
+    }
+}
+// qs for outputs
+class secClass {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *" );
+        System.out.println("* * *");
+        System.out.println("* * * *");
+
+
+    }
+}
+
