68766e0f9b267bd80939ba68f82f1071ef42a930
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-09 02:21:50 +0600

    only prite even number

--- day4/index.java
+++ day4/index.java
@@ -5,3 +5,11 @@ class firstClass{
         }
     }
 }
+// This example will only print even values between 0 and 10:
+class secondClasss{
+    public static void main(String[] args){
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+          }
+    }
+}
