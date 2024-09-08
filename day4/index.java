a7c5c891a605b0ead77ab6700e5a85be981588b7
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-09 02:24:16 +0600

    nested for loop

--- day4/index.java
+++ day4/index.java
@@ -13,3 +13,18 @@ class secondClasss{
           }
     }
 }
+
+// This example will only print even values between 0 and 10:
+class thirdClass{
+    public static void main(String[] args){
+        // Outer loop
+for (int i = 1; i <= 2; i++) {
+    System.out.println("Outer: " + i); // Executes 2 times
+    
+    // Inner loop
+    for (int j = 1; j <= 3; j++) {
+      System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+    }
+  } 
+    }
+}
