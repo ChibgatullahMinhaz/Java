a0430528a6a2331912ebdda345faafadbdc74c98
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-10 00:29:28 +0600

    InvertedRightAngleTriangle

--- pettern_Question/InvertedRightAngleTriangle.java
+++ pettern_Question/InvertedRightAngleTriangle.java
@@ -1,12 +1,11 @@
-public class Pattern1 {
+public class InvertedRightAngleTriangle {
     public static void main(String[] args) {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
+        for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); 
+            System.out.println(); // Move to the next line
         }
     }
 }
-
