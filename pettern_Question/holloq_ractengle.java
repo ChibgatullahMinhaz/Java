008b56f2d3f465e44f00894a60f3a665a68fc0ca
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-12 00:59:08 +0600

    changes

--- pettern_Question/holloq_ractengle.java
+++ pettern_Question/holloq_ractengle.java
@@ -5,9 +5,9 @@ class holloq_ractengle {
     for (int i = 1; i <=n; i++) {
         for (int j = 1; j <=m; j++) {
             if (i== 1 ||i == n || j == 1  || j == m) {
-                System.out.print(" * ");
+                System.out.print("*");
             }else{
-                System.out.print( "   ");
+                System.out.print( " ");
             }
         }
         System.out.println();
