bdce1bb6d1748f1087c45a7df1c43454436786a3
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-17 14:38:56 +0600

    grad outpt

---
+++ pettern_Question/grad.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Grade {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the student's Mark (out of 100):");
+
+        int Mark = scanner.nextInt();  
+        
+        String grade;
+
+        switch (Mark / 10) {
+            case 10:
+            case 9:
+            case 8:
+                grade = "A+";
+                break;
+            case 7:
+                grade = "A";
+                break;
+            case 6:
+                grade = "B";
+                break;
+            case 5:
+                grade = "C";
+                break;
+            case 4:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+        }
+
+        System.out.println("Grade: " + grade);  
+        scanner.close();
+    }
+}
