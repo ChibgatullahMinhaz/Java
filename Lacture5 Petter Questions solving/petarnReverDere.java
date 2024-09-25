3f8d5be8a19bbe41c35c299bb0f33b61c140a304
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-25 22:00:10 +0600

    changes

--- Lacture5 Petter Questions solving/petarnReverDere.java
+++ Lacture5 Petter Questions solving/petarnReverDere.java
@@ -1,9 +1,9 @@
+import java.util.*;
 class petarnReverDere {
-
     public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
 
-        int n = 4;
-
+        int n = scanner.nextInt();
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j < i; j++) {
                 System.out.print(" ");
@@ -14,6 +14,7 @@ class petarnReverDere {
             }
             System.out.println();
         }
+        scanner.close();
     }
 
 }
