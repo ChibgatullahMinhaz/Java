297cd1d7b3f6e598fdb0f2f39017e63e2b541e0e
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-28 00:02:48 +0600

    changes

--- Function Questions/CountNumbers.java
+++ Function Questions/CountNumbers.java
@@ -3,28 +3,26 @@ import java.util.Scanner;
 public class CountNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int positiveCount = 0;
         int negativeCount = 0;
         int zeroCount = 0;
-        
+
         while (true) {
             System.out.print("Enter a number (or type 'exit' to finish): ");
-            String input = sc.nextLine(); 
-            
+            String input = sc.nextLine();
+
             if (input.equalsIgnoreCase("exit")) {
-                break; 
+                break;
             }
-            
             try {
                 int number = Integer.parseInt(input);
-                
                 if (number > 0) {
                     positiveCount++;
                 } else if (number < 0) {
-                    negativeCount++; 
+                    negativeCount++;
                 } else {
-                    zeroCount++; 
+                    zeroCount++;
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Please enter a valid number.");
@@ -34,7 +32,6 @@ public class CountNumbers {
         System.out.println("Total Positive Numbers: " + positiveCount);
         System.out.println("Total Negative Numbers: " + negativeCount);
         System.out.println("Total Zeros: " + zeroCount);
-
         sc.close();
     }
 }
