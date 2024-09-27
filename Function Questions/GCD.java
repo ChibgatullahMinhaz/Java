17f451fba5adf7e126e4b1b48c08f903ed0d3dfb
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-09-28 00:52:30 +0600

    gratest common devisor

---
+++ Function Questions/GCD.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class GCD {
+    public static int getGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return getGCD(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print("gcd is : " + getGCD(a, b));
+        sc.close();
+    }
+}
