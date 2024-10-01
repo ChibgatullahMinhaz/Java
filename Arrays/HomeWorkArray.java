e3b80a9033e21aa7ba1670254d440325d4480836
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-10-01 20:52:15 +0600

    homwork

---
+++ Arrays/HomeWorkArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class HomeWorkArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        String names[] = new String[size];
+        // inputs
+        for (int i = 0; i <= size; i++) {
+            names[i] = scanner.next();
+        }
+        // outputs
+         for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+         }
+        scanner.close();
+    }
+}
