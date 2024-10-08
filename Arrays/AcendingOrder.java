34bd07d954ba634509ae3218318eb0e558699567
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-10-08 21:27:28 +0600

    changes

--- Arrays/AcendingOrder.java
+++ Arrays/AcendingOrder.java
@@ -1,15 +1,12 @@
 import java.util.*;
-
 public class AcendingOrder {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int numbers[] = new int[size];
-
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
         }
-
         boolean isAscending = true;
 
         for (int i = 0; i < numbers.length - 1; i++) { 
