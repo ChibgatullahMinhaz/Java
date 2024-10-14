215fdfaea228d90be7a6d318976127f76e0f3789
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-10-14 22:27:22 +0600

    fixedd

--- Arrays/AcendingOrder.java
+++ Arrays/AcendingOrder.java
@@ -7,8 +7,8 @@ public class AcendingOrder {
         for (int i = 0; i < size; i++) {
             numbers[i] = sc.nextInt();
         }
+        
         boolean isAscending = true;
-
         for (int i = 0; i < numbers.length - 1; i++) { 
             if (numbers[i] > numbers[i + 1]) {
                 isAscending = false;
