71e40b3bef7e10e287d33c8f934235c10ba63034
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-12 00:36:17 +0600

    constructor

---
+++ day6/contractor.java
@@ -0,0 +1,21 @@
+public class contractor{
+    public static void main(String[] args){
+        Student s = new Student();
+    }
+}
+
+class Student{
+int roll ;
+String name;
+int age;
+public Student() {
+roll = 1;
+name = "John";
+age = 20;
+System.out.println("Roll No: "+roll);
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+}  
+
+}
+
